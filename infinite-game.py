8310ff5442ee320bbf948b718da674be789aa583
pseducode <10664638+pseducode@users.noreply.github.com>
2022-11-25 16:49:48 -0500

    add init files,, as placeholders

---
+++ infinite-game.py
@@ -0,0 +1,23 @@
+#basic python implementation of wordle, using actual wordle, word-list,
+#except with infinite guesses, insteasd of just 6, for poractice.
+#need to use pygame for display purposess
+import random
+import os, sys
+import fnmatch
+import pygame
+
+
+def main():
+    print ("main game loop")
+    #first load wordlist & pick one at random
+    #load in graphics
+    #prompt usaer to type in guess
+    #compare guess to selected
+    #display results of guess onscreen
+
+
+if __name__=="__main__":
+    main()
+
+
+
